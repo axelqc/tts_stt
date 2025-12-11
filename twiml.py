f9e99cdecc204f4f63f1f7217012633c96480340
Axel Quiroga Caldera <87676595+axelqc@users.noreply.github.com>
2025-12-11 12:08:37 -0600

    Update twiml.py

--- twiml.py
+++ twiml.py
@@ -1,9 +1,12 @@
-# twiml.py
+# twiml.py - Compatible con recording_manager
 def twiml_response(host):
 
     return f"""<?xml version="1.0" encoding="UTF-8"?>
 <Response>
     <Say language="es-MX">Conectando, por favor espera.</Say>
+    <Connect>
+        <Stream url="wss://{host}/media-stream" />
+    </Connect>
     <Record 
         recordingStatusCallback="https://{host}/recording-status"
         recordingStatusCallbackMethod="POST"
@@ -11,7 +14,4 @@ def twiml_response(host):
         playBeep="false"
         transcribe="false"
     />
-    <Connect>
-        <Stream url="wss://{host}/media-stream" track="inbound_track" />
-    </Connect>
 </Response>"""
